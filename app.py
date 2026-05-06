c4bec77099bc1f511eb9ebd1be88476868aba3b5
9703698974 <charankumar8030@gmail.com>
2026-05-06 10:28:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 
-def add(a,b):
-  return a+b
+def add(a, b):
+    return a + b
 
-  
-if __name__=="__main__":
-  result = add(2,3)
-  print(result)
+
+if __name__ == "__main__":  # pragma: no cover
+    result = add(2, 3)
+    print(result)
